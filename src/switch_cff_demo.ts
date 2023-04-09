6d258087bedae9f09f19bb2a384f4211a06d0962
hans774882968 <774882968@qq.com>
2023-04-09 20:10:38 +0800

    style: 去除重复逻辑

--- src/switch_cff_demo.ts
+++ src/switch_cff_demo.ts
@@ -8,28 +8,12 @@ import traverse from '@babel/traverse';
 import {
   Node,
   isIdentifier,
-  isMemberExpression,
-  isObjectExpression,
-  isObjectProperty,
-  isStringLiteral,
   isFunctionExpression,
-  isReturnStatement,
-  isBinaryExpression,
-  binaryExpression,
-  isLogicalExpression,
-  logicalExpression,
-  isCallExpression,
-  callExpression,
-  isExpression,
   isNumericLiteral,
-  stringLiteral,
-  isBlockStatement,
-  isSwitchStatement,
-  isVariableDeclarator,
-  isContinueStatement,
-  Statement,
-  isUpdateExpression
+  stringLiteral
 } from '@babel/types';
+import { switchCFF } from './remove_switch_cff';
+import { cff } from './remove_cff';
 
 const jsCode = getFile('src/inputs/switch_cff_demo.js');
 const ast = parser.parse(jsCode);
@@ -62,137 +46,8 @@ restoreStringLiteral(ast, ['_0x27c4'], (idx: number) => {
   return ['661835nuUXrL', 'dKifE', 'try again', 'log', '7aEbwep', 'awvtQ', '2804302XtaWgC', 'rmnID', 'flag{hans}', '21393471OyFTzd', 'lXUhG', '1914456NQDFwp', '1xRwaZJ', '36ZbcbZP', '3gJgrjU', '8162226GwaJpl', '3|4|2|0|5|1', 'split', 'charCodeAt', 'pass', '6278120IHpVNF', 'W_PTJ[P]BN', 'length', 'fromCharCode', '939280gOLaZV'][idx - 0x89];
 });
 
-function cff (ast: Node) {
-  type ASTNodeMap = {[key: string]: Node}
-  const cffTables: {[key: string]: ASTNodeMap} = {};
-  traverse(ast, {
-    VariableDeclarator (path) {
-      const node = path.node;
-      if (!node.id || !isIdentifier(node.id)) return;
-      const tableName = node.id.name;
-      if (!isObjectExpression(node.init)) return;
-      const tableProperties = node.init.properties;
-      cffTables[tableName] = tableProperties.reduce((cffTable, tableProperty) => {
-        if (!isObjectProperty(tableProperty) ||
-           !isStringLiteral(tableProperty.key)) return cffTable;
-        cffTable[tableProperty.key.value] = tableProperty.value;
-        return cffTable;
-      }, {} as ASTNodeMap);
-    }
-  });
-
-  traverse(ast, {
-    CallExpression (path) {
-      const cNode = path.node;
-      if (isMemberExpression(cNode.callee)) {
-        if (!isIdentifier(cNode.callee.object)) return;
-        const callParams = cNode.arguments;
-        const tableName = cNode.callee.object.name;
-        if (!isStringLiteral(cNode.callee.property)) return;
-        const keyName = cNode.callee.property.value;
-        if (!(tableName in cffTables) ||
-            !(keyName in cffTables[tableName])) return;
-        const shouldBeFuncValue = cffTables[tableName][keyName];
-        if (!isFunctionExpression(shouldBeFuncValue) ||
-            !shouldBeFuncValue.body.body.length ||
-            !isReturnStatement(shouldBeFuncValue.body.body[0])) return;
-        // 拿到返回值
-        const callArgument = shouldBeFuncValue.body.body[0].argument;
-        if (isBinaryExpression(callArgument) && callParams.length === 2) {
-          if (!isExpression(callParams[0]) || !isExpression(callParams[1])) {
-            throw '二元运算符中，两个参数都应为表达式';
-          }
-          // 处理function(x, y){return x + y}这种形式
-          path.replaceWith(binaryExpression(callArgument.operator, callParams[0], callParams[1]));
-        } else if (isLogicalExpression(callArgument) && callParams.length === 2) {
-          if (!isExpression(callParams[0]) || !isExpression(callParams[1])) {
-            throw '逻辑运算符中，两个参数都应为表达式';
-          }
-          // 处理function(x, y){return x > y}这种形式
-          path.replaceWith(logicalExpression(callArgument.operator, callParams[0], callParams[1]));
-        } else if (isCallExpression(callArgument) && isIdentifier(callArgument.callee)) {
-          // 处理function(f, ...args){return f(...args)}这种形式
-          if (callParams.length == 1) {
-            path.replaceWith(callParams[0]);
-          } else {
-            if (!isExpression(callParams[0])) {
-              throw '仅支持第一个参数为函数的形式，如：function(f, ...args){return f(...args)}';
-            }
-            path.replaceWith(callExpression(callParams[0], callParams.slice(1)));
-          }
-        }
-      }
-    },
-    MemberExpression (path) {
-      const mNode = path.node;
-      if (!isIdentifier(mNode.object)) return;
-      const tableName = mNode.object.name;
-      if (!isStringLiteral(mNode.property)) return;
-      const keyName = mNode.property.value;
-      if (!(tableName in cffTables) ||
-          !(keyName in cffTables[tableName])) return;
-      const cffTableValue = cffTables[tableName][keyName];
-      path.replaceWith<Node>(cffTableValue);
-    }
-  });
-}
 cff(ast);
 
-function switchCFF (ast: Node) {
-  traverse(ast, {
-    WhileStatement (path) {
-      const wNode = path.node;
-      if (!isBlockStatement(wNode.body) || !wNode.body.body.length) return;
-      const switchNode = wNode.body.body[0];
-      if (!isSwitchStatement(switchNode)) return;
-      const { discriminant, cases } = switchNode;
-      if (!isMemberExpression(discriminant) ||
-          !isIdentifier(discriminant.object)) return;
-      // switch语句内的控制流平坦化数组名，本例中是 _0x31ce85
-      const arrayName = discriminant.object.name;
-      // 获取控制流平坦化数组绑定的节点
-      const bindingArray = path.scope.getBinding(arrayName);
-      if (!bindingArray) return;
-      // 经过restoreStringLiteral，我们认为它已经恢复为'v1|v2...'['split']('|')
-      if (!isVariableDeclarator(bindingArray.path.node) ||
-          !isCallExpression(bindingArray.path.node.init)) return;
-      const varInit = bindingArray.path.node.init;
-      if (!isMemberExpression(varInit.callee) ||
-          !isStringLiteral(varInit.callee.object) ||
-          varInit.arguments.length !== 1 ||
-          !isStringLiteral(varInit.arguments[0])) return;
-      const object = varInit.callee.object.value;
-      const propty = varInit.callee.property;
-      if (!isStringLiteral(propty) && !isIdentifier(propty)) return;
-      const propertyName = isStringLiteral(propty) ? propty.value : propty.name;
-      const splitArg = varInit.arguments[0].value;
-      // 目前只支持'v1|v2...'.split('|')的解析
-      if (propertyName !== 'split') {
-        console.warn('switchCFF(ast)：目前只支持\'v1|v2...\'.split(\'|\')的解析');
-        return;
-      }
-      const indexArr = object[propertyName](splitArg);
-
-      const replaceBody = indexArr.reduce((replaceBody, index) => {
-        const caseBody = cases[+index].consequent;
-        if (isContinueStatement(caseBody[caseBody.length - 1])) {
-          caseBody.pop();
-        }
-        return replaceBody.concat(caseBody);
-      }, [] as Statement[]);
-      path.replaceInline(replaceBody);
-
-      // 可选择的操作：删除控制流平坦化数组绑定的节点、自增变量名绑定的节点
-      if (!isUpdateExpression(discriminant.property) ||
-          !isIdentifier(discriminant.property.argument)) return;
-      const autoIncrementName = discriminant.property.argument.name;
-      const bindingAutoIncrement = path.scope.getBinding(autoIncrementName);
-      if (!bindingAutoIncrement) return;
-      bindingArray.path.remove();
-      bindingAutoIncrement.path.remove();
-    }
-  });
-}
 switchCFF(ast);
 
 function removeStringTransCodes (ast: Node) {
